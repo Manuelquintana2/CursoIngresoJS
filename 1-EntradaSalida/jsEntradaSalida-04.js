c52991566592e01f88d0c700abc27f55edea6d12
Manuelquintana2 <118947022+Manuelquintana2@users.noreply.github.com>
2023-01-12 17:16:07 -0300

    vario cambios

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,19 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	//reservo memoria para guardar la variable nombre
+
+	let nombre;
+
+	// guardo en la variable nombre el texto que escriba el usuario en el prompt
+
+	nombre = prompt ("ingrese su nombre");
+
+//copio el nombre guardado en la variable dentro de la caja de texto embebida en la pagina html
+
+document.getElementById("txtIdNombre").value  = nombre;
+
+
+alert(nombre)
 }
 
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,38 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	
+	//let x = "20";
+
+	//x = parseInt(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	let num1;
+	let num2;
+	let resultado;
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+
+	num2 = document.getElementById("txtIdNumeroDos").value;
+
+	num1 = parseInt(num1);
+
+	num2 = parseInt(num2);
+
+	resultado = num1 + num2;
+
+	alert("la suma es " + resultado);
 }
 
